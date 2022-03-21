bc0aa6943f8998583de79fc3b8eb466c41dd1396
Eric Huang <huangeric.j@gmail.com>
2022-03-21 09:28:02 -0700

    Cleaned up mongoDB connection code

--- config/index.js
+++ config/index.js
@@ -3,7 +3,6 @@ module.exports = {
   port: process.env.PORT || 5000,
   db: {
     mongoUrl: process.env.MONGO_URI,
-    mongoDevUrl: process.env.MONGO_DEV_URI,
   },
   jwtConfig: {
     secret: process.env.JWT_SECRET,
