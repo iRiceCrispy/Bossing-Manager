9de1f2117b4b6b5ed83d78e4779665187b772ed4
Eric Huang <huangeric.j@gmail.com>
2022-03-24 10:20:22 -0700

    Home button removes context instead of redirect

--- client/src/components/Navigation/index.jsx
+++ client/src/components/Navigation/index.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import { useSelected } from '../../context/SelectedContext';
 import Modal from '../Modal';
 import PartyForm from '../Forms/PartyForm';
 import ProfileButton from './ProfileButton';
@@ -8,13 +9,23 @@ import './Navigation.css';
 
 const Navigation = () => {
   const [showCreate, setShowCreate] = useState(false);
+  const { setSelectedParty, setSelectedDrop } = useSelected();
   const user = useSelector(state => state.session.user);
 
   return (
     <nav>
       <div className='navbar'>
         <div className='home'>
-          <a className='btn transparent' href='/'>Home</a>
+          <Link
+            className='btn transparent'
+            to='/'
+            onClick={() => {
+              setSelectedParty('');
+              setSelectedDrop('');
+            }}
+          >
+            Home
+          </Link>
         </div>
         <div className='options'>
           {showCreate ? (
